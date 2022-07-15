d9edcd0b518795eb7a9e12901d288b39cf449adf
abebe21 <abbebbe@gmail.com>
2022-07-15 06:06:39 -0700

    rewrking on arrays adn pointers in c languages

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,23 @@
+#include"main.h"
+
+/**
+ * _strcat->function to concat string
+ *
+ *@dest:first parameter
+ *@src:second parameter
+ *
+ * return : string
+ */
+char *_strcat(char *dest, char *src){
+	int l,i;
+
+	while(dest[l])
+	       	l++;
+	for(i=0;src[i]!=0;i++){
+	dest[l]=src[i];
+	l++;
+	}
+	dest[l]='\0';
+	return (dest);
+
+}
