18a077eef0162052516a94cec42244929ac6875e
abebe21 <abbebbe@gmail.com>
2022-07-15 07:14:52 -0700

    correction for betty styles

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,23 +1,25 @@
-#include"main.h"
+#include "main.h"
 
 /**
- * _strcat->function to concat string
- *
- *@dest:first parameter
- *@src:second parameter
- *
- * return : string
- */
-char *_strcat(char *dest, char *src){
-	int l,i;
+* _strcat ->  function to concat string
+*
+*@dest:first parameter
+*@src:second parameter
+*
+*return:string
+*/
+char *_strcat(char *dest, char *src)
+{
+	int l, i;
 
-	while(dest[l])
-	       	l++;
-	for(i=0;src[i]!=0;i++){
-	dest[l]=src[i];
+	while (dest[l])
+		l++;
+	for (i = 0; src[i] != 0; i++)
+	{
+	dest[l] = src[i];
 	l++;
 	}
-	dest[l]='\0';
-	return (dest);
 
+	dest[l] = '\0';
+	return (dest);
 }
