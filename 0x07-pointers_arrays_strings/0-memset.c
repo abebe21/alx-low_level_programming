31f6460d7a80f73c8cb67a3efd0bfd8d2c0c623b
abebe21 <abbebbe@gmail.com>
2022-07-18 04:17:12 -0700

    betty code style fixing

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -4,17 +4,18 @@
  * _memset - a function to append n number of character to string s
  *
  *@s: String aprameter
- *@b: string to be concat
- *@n:number of characters
+ *@b: character to be concat
+ *@n: number of characters
  *
  *Return: String
  */
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int a;
-	for (a = 0;a < n; a++)
-	{
+
+	for (a = 0; a < n; a++)
+
 		s[a] = b;
-	}
+
 	return (s);
 }
