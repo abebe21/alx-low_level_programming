bfb65fcfdb4e1df08503a41787f3aff696c62329
abebe21 <abbebbe@gmail.com>
2022-07-18 02:46:08 -0700

    the fisrt execise is done

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _memset - a function to append n number of character to string s
+ *
+ *@s: String aprameter
+ *@b: string to be concat
+ *@n:number of characters
+ *
+ *Return: String
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int a;
+	for(a=0;a<n;a++)
+	{
+		s[a]=b;
+	}
+	return (s);
+}
