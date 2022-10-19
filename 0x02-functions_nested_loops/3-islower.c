9596faa4ee76e4ce2906af3e95ac0377cb692532
Abebe Manaye <abbebbe@gmail.com>
2022-10-19 06:40:38 -0700

    adding file

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main"
+/**
+ *
+ * *_islower - function to check for lowercase character
+ *
+ * *@c: print alphbet
+ *
+ * *Return: 1 if c is lowercase, otherwise 0
+ *
+ **/
+
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}
