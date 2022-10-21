a27a0939e62631dc607926f3f460b9012c0a6972
Abebe Manaye <abbebbe@gmail.com>
2022-10-21 06:19:40 -0700

    adding more

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -4,7 +4,6 @@
  *  main - causes an infinite loop
  *
  *  Return: 0
- *
  **/
 
 int main(void)
@@ -12,7 +11,6 @@ int main(void)
 	int i;
 
 	printf("Infinite loop incoming :(\n");
-
 	i = 0;
 	/**
 	*while (i < 10)
--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -3,17 +3,13 @@
 
 /**
  * print_remaining_days - takes a date and prints how many days are
- *
  * left in the year, taking leap years into account
  *
  * @month: month in number format
- *
  * @day: day of month
- *
  * @year: year
  *
  * Return: void
- *
  **/
 
 void print_remaining_days(int month, int day, int year)
