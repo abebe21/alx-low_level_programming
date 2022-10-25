598423f4dc6138ee8e022f692d135c7101b4fbdb
Abebe Manaye <abbebbe@gmail.com>
2022-10-25 01:05:47 -0700

    adding more exerc

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -9,12 +9,13 @@
 void puts2(char *str)
 {
 	int count = 0;
-	
+
 	while (count >= 0)
 	{
 		if (str[count] == '\0')
 		{
-			_putchar('\n');																break;
+			_putchar('\n');
+			break;
 		}
 		
 		if (count % 2 == 0)
