10f34906c61c027ddee1f5e3eacd09a837de7df9
Abebe Manaye <abbebbe@gmail.com>
2022-10-25 03:43:18 -0700

    add more exe3

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,35 +1,27 @@
 #include "main.h"
 
 /**
+ * _strcpy - Copy paste string
  *
- * * _strcpy - Copy paste string
+ * @dest: destination
  *
- * *@dest: destination
+ * @src: source
  *
- * *@src: source
+ * Return: dest
  *
- * *Return: dest
- *
- * */
+ **/
 
 char *_strcpy(char *dest, char *src)
-
 {
-
 	int inc = 0;
 
 	while (*(src + inc) != '\0')
-
 	{
-
 		*(dest + inc) = *(src + inc);
-
 		inc++;
-
 	}
-
+	
 	*(dest + inc) = '\0';
-
+	
 	return (dest);
-
 }
